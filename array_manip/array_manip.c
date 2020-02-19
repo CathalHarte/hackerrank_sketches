a065b64248dbbd607cf1ae87896a894030084bc6
CathalHarte <cathal.harte@rapttouch.com>
2020-02-19 18:27:39 +0100

    This might be right. Next step is to unit test the short versus the long more reliable version

--- array_manip/array_manip.c
+++ array_manip/array_manip.c
@@ -10,13 +10,22 @@
 
 #include <time.h>
 
+#define TEN_MILLION     10000000
 clock_t start_time, end_time;
 double cpu_time_used;
      
 char* readline();
 char** split_string(char*);
 
-uint64_t tally[10000000];
+uint64_t tally[TEN_MILLION];
+
+typedef struct linked_range
+{
+    uint64_t val;
+    uint64_t upprange; // upprange is the location of the first index in the array which is not "val"
+} linked_range_t;
+
+linked_range_t linked_range_array[20];
 
 // Complete the arrayManipulation function below.
 
@@ -60,6 +69,88 @@ uint64_t arrayManipulation(int n, int operations_rows, int operations_columns, i
     // 2510535321
 }
 
+void init_linked_range_array(void)
+{
+    linked_range_array[0].upprange = 20;
+}
+// The operations matrix is an m by 3 matrix
+// This is the linkedList style of solution
+//      It can save on huge number of computations by having a data structure that defines ranges of values
+uint64_t arrayManipulationLL(int n, int operations_rows, int operations_columns, int** operations) 
+{
+    uint64_t max = 0;
+    uint64_t op_add;
+    uint64_t op_lowrange, op_upprange;
+
+    int lowrange_to_add;
+    int upprange_to_add;
+    int preexist_upprange;
+    int preexist_val;
+
+    start_time = clock();
+    int ** last_operation_row = &(operations[operations_rows]);
+
+    int operation_idx = 0;
+    for(; operations<last_operation_row; operations++)
+    {
+        operation_idx++;
+        op_lowrange = operations[0][0]-1;
+        op_upprange = operations[0][1];
+        op_add = operations[0][2];
+
+        int linked_range_idx = 0;
+        while(linked_range_idx < TEN_MILLION)
+        {
+            if(op_lowrange >= linked_range_array[linked_range_idx].upprange )
+            {
+                linked_range_idx = linked_range_array[linked_range_idx].upprange;
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+
+        // the start val has been found
+        // next we need to add subranges iteratively as necessary
+
+        lowrange_to_add = op_lowrange;
+        upprange_to_add = op_upprange;
+
+        while(linked_range_idx < TEN_MILLION)
+        {
+            preexist_upprange = linked_range_array[linked_range_idx].upprange;
+            preexist_val = linked_range_array[linked_range_idx].val;
+
+            linked_range_array[lowrange_to_add].val = preexist_val + op_add;
+            if(preexist_upprange >= upprange_to_add)
+            {
+                linked_range_array[linked_range_idx].upprange = lowrange_to_add; // on a second loop, this value is overwritten
+                linked_range_array[lowrange_to_add].upprange = upprange_to_add;
+                linked_range_array[upprange_to_add].val = preexist_val;
+                linked_range_array[upprange_to_add].upprange = preexist_upprange;
+
+                break;
+            }
+            else
+            {
+                linked_range_array[lowrange_to_add].upprange = preexist_upprange;
+                lowrange_to_add = preexist_upprange;
+                linked_range_idx = preexist_upprange;
+            }
+            
+            
+        }
+    }
+
+    end_time = clock();
+
+    return max;
+    // 7515267971
+    // 2510535321
+}
+
 int main()
 {
     FILE *fp = NULL;
@@ -67,7 +158,7 @@ int main()
     int m; // number of operations
 
     /* 1. Open file for Reading */                                                 
-    if (NULL == (fp = fopen("../failing_test_case_2.tsv","r")))                                   
+    if (NULL == (fp = fopen("../failing_test_case_4.tsv","r")))                                   
     {                                                                           
         perror("Error while opening the file.\n");                              
         exit(EXIT_FAILURE);                                                     
@@ -99,7 +190,8 @@ int main()
         }
     }
 
-    long result = arrayManipulation(n, m, 3, operations);
+    init_linked_range_array();
+    long result = arrayManipulationLL(n, m, 3, operations);
     
     cpu_time_used = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
     
